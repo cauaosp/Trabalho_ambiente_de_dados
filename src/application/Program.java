92fef19bde46e3a6e75f8be424ab63760361fad1
cauaosp <cauaosp20@gmail.com>
2025-05-27 19:49:51 -0300

    teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd");
+		System.out.println("testando!");
 		
 		//AlunoDao alunodao = AlunoFactory.createAlunoDao();
 		
